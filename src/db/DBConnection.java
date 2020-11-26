c62b8d05a951a8a29a11a5c2ddb6eb6bf69baef1
Zac <zhartzog400@gmail.com>
2020-11-26 00:16:38 -0600

    Unit test for getting all events by username

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -20,7 +20,19 @@ public class DBConnection extends HttpServlet {
 	
 	static Connection connection = null;
 	
-	public DBConnection(ServletContext context) {
+	public DBConnection() {
+	}
+		
+	public void setConnection(String url, String user, String password) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getConnectionByProperties(ServletContext context) {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 			UtilProp.loadProperty(context);
@@ -31,7 +43,7 @@ public class DBConnection extends HttpServlet {
 			return;
 		}
 	}
-		
+	
 	public Connection getConnection() {
 		return connection;
 	}
